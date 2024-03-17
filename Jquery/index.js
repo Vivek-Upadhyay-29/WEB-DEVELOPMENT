18a2f8f78684c335257ec7a589c42f8aede8dbf4
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-03-17 23:54:13 +0530

    commit

---
+++ Jquery/index.js
@@ -0,0 +1,20 @@
+
+// $("h1").click(function(){
+//
+// });
+
+    $("button").on("click" , function(){
+        $("h1").slideUp();
+    })
+    // $("button").on("mouseover", function(){
+    //     $("h1").fadeOut();
+    // })
+
+$("input").keypress(function(event){
+    $("h1").text(event.key).addClass("has-class margin");
+  
+ 
+    
+})
+
+$
