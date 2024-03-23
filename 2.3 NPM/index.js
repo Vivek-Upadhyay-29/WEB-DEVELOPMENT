701002cd405c3efcefa54f414acdd47f500e88c5
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-03-24 00:20:36 +0530

    commit

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,7 @@
+// import superheros from "superheros";
+// const sSname = superheros.random();
+// console.log(`I am ${sSname}!`)
+
+import superheroes from "superheroes";
+let mySuperheroName = superheroes.random();
+console.log(mySuperheroName)
