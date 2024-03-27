10def1ab31c80e9da307157a37d6de9cb6e2f505
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-03-27 20:41:08 +0530

    changes

--- Backend/practicing http/index.js
+++ Backend/practicing http/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.listen(port, ()=>{
-    console.log(`server started ${port}`);
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
