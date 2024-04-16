bf5fffcef7ab7cb0a938f03cd0506896275a9b0c
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-04-16 21:38:56 +0530

    changes

--- Jquery/index.js
+++ Jquery/index.js
@@ -6,17 +6,20 @@
     $("button").on("click" , function(){
         $("h1").slideUp();
     })
-    // $("button").on("mouseover", function(){
-    //     $("h1").fadeOut();
-    // })
+    $("button").on("mouseover", function(){
+        $("h1").fadeOut();
+    })
 
 $("input").keypress(function(event){
     $("h1").text(event.key).addClass("has-class margin");
   
  
     
-});
+// });
 
-$("h1").on("click",function(){
-    $("h1").fadeOut();
-});
+// $("h1").on("click",function(){
+//     $("h1").fadeOut();
+// });
+// $("button").Click( "click" , function(){
+// $("h1").addClass("has-class margin");
+// });
