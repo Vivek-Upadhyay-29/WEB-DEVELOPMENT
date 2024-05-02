695d9b171cbce1def0b346c91b3f4b55aedce7ee
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-05-02 22:45:36 +0530

    commit

--- Backend/3.4 Middleware/index2.js
+++ Backend/3.4 Middleware/index2.js
@@ -1,9 +1,9 @@
 import express from "express";
-import bodyParser from "body-parser";
-
+import morgan from "morgan";
 const app = express();
 const port = 3000;
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
